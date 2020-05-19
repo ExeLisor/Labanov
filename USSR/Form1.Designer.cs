ba1fca99d4fe6ccd05b3cbd785ecbacc9cbe65e2
Alexander <sanin.sanek2019@gmail.com>
2020-05-19 11:32:52 +0300

    USSR

---
+++ USSR/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace USSR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.TBspeed = new System.Windows.Forms.TextBox();
+            this.TBangle = new System.Windows.Forms.TextBox();
+            this.TBresistance = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.BtStart = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtStop = new System.Windows.Forms.Button();
+            this.BtReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1267, 317);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // TBspeed
+            // 
+            this.TBspeed.Location = new System.Drawing.Point(11, 419);
+            this.TBspeed.Name = "TBspeed";
+            this.TBspeed.Size = new System.Drawing.Size(100, 22);
+            this.TBspeed.TabIndex = 1;
+            // 
+            // TBangle
+            // 
+            this.TBangle.Location = new System.Drawing.Point(117, 420);
+            this.TBangle.Name = "TBangle";
+            this.TBangle.Size = new System.Drawing.Size(100, 22);
+            this.TBangle.TabIndex = 2;
+            // 
+            // TBresistance
+            // 
+            this.TBresistance.Location = new System.Drawing.Point(223, 420);
+            this.TBresistance.Name = "TBresistance";
+            this.TBresistance.Size = new System.Drawing.Size(100, 22);
+            this.TBresistance.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // BtStart
+            // 
+            this.BtStart.Location = new System.Drawing.Point(834, 387);
+            this.BtStart.Name = "BtStart";
+            this.BtStart.Size = new System.Drawing.Size(136, 57);
+            this.BtStart.TabIndex = 4;
+            this.BtStart.Text = "Старт";
+            this.BtStart.UseVisualStyleBackColor = true;
+            this.BtStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(114, 400);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Угол";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(220, 400);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Масса";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 399);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Скорость";
+            // 
+            // BtStop
+            // 
+            this.BtStop.Location = new System.Drawing.Point(976, 387);
+            this.BtStop.Name = "BtStop";
+            this.BtStop.Size = new System.Drawing.Size(136, 57);
+            this.BtStop.TabIndex = 9;
+            this.BtStop.Text = "Стоп";
+            this.BtStop.UseVisualStyleBackColor = true;
+            this.BtStop.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // BtReset
+            // 
+            this.BtReset.Location = new System.Drawing.Point(1118, 387);
+            this.BtReset.Name = "BtReset";
+            this.BtReset.Size = new System.Drawing.Size(136, 57);
+            this.BtReset.TabIndex = 12;
+            this.BtReset.Text = "Перезапуск";
+            this.BtReset.UseVisualStyleBackColor = true;
+            this.BtReset.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1266, 450);
+            this.Controls.Add(this.BtReset);
+            this.Controls.Add(this.BtStop);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BtStart);
+            this.Controls.Add(this.TBresistance);
+            this.Controls.Add(this.TBangle);
+            this.Controls.Add(this.TBspeed);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox TBspeed;
+        private System.Windows.Forms.TextBox TBangle;
+        private System.Windows.Forms.TextBox TBresistance;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button BtStart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtStop;
+        private System.Windows.Forms.Button BtReset;
+    }
+}
+
---
+++ USSR/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace USSR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        float x = 50;
+        float y = 200;
+        float x0 = 50;
+        float y0 = 200;
+        float v = 0;
+        float vx;
+        float vy;
+        float fx;
+        float fy;
+        float ax;
+        float ay;
+        float ang;
+        float t = 0;
+        float t0 = 0;
+        float m;
+        float g = 9.81f;
+        float k = 2f;
+        float zemlya = 201;
+        double pi = Math.PI / 180;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            vy = (float)(v * Math.Sin(ang * pi));
+            vx = (float)(v * Math.Cos(ang * pi));
+            fx = -k * vx * vx;
+            fy = -(m * g + k * v * vy);
+            t++;
+            float dt = t / 100;
+            ax = fx / m;
+            ay = fy / y;
+            vx = vx + ax * dt;
+            vy = vy + ay * dt;
+            y -= vx + ax * dt * dt / 2;
+            x += vy - ay * dt * dt / 2;
+            if (y >= zemlya)
+            {
+                timer1.Stop();
+            }
+            pictureBox1.Refresh();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            float.TryParse(TBspeed.Text, out v);
+            float.TryParse(TBangle.Text, out ang);
+            float.TryParse(TBresistance.Text, out m);
+            timer1.Start();
+            timer1.Interval = 10;           
+        }
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillEllipse(Brushes.Black, x, y, 20, 20);
+            e.Graphics.DrawLine(Pens.Black, 60, 250, 60, 10);
+            e.Graphics.DrawLine(Pens.Black, 40, 210, 900, 210);
+            e.Graphics.DrawLine(Pens.Black, 60, 10, 55, 30);
+            e.Graphics.DrawLine(Pens.Black, 60, 10, 65, 30);
+            e.Graphics.DrawLine(Pens.Black, 900, 210, 880, 205);
+            e.Graphics.DrawLine(Pens.Black, 900, 210, 880, 215);
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            timer1.Stop();        
+            x = x0;
+            y = y0;
+            t = t0;           
+            pictureBox1.Refresh();
+        }
+
+        
+    }
+}
+
